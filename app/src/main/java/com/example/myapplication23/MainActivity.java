e382bb0799393bff163c1c6dad04bbe7e4c99ab7
Shantanu Hada <shantanuhada118@gmail.com>
2023-05-19 14:37:56 +0530

    First

---
+++ app/src/main/java/com/example/myapplication23/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.myapplication23;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+//    private Button button;
+//    private TextView textview;
+//    private EditText editTextTextPersonName;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        button = findViewById(R.id.button);
+//        textview = findViewById(R.id.textView);
+//        editTextTextPersonName = findViewById(R.id.editTextTextPersonName);
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Toast.makeText(MainActivity.this,"party chaiye mereko", Toast.LENGTH_SHORT).show();
+//                String s  = editTextTextPersonName.getText().toString();
+//                int kg = Integer.parseInt(s);
+//                double pound= 2.205*kg;
+//                textview.setText("the corresponding value is compound is" + pound);
+            }
+        });
+}
